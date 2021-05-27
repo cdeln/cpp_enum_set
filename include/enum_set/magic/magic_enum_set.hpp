0d26503e070ed39a9eef1bef5834b6581add04ec
cdeln <carl@dehlin.com>
2021-05-27 07:54:51 +0200

    Fix CI errors for Windows
      - Break up complex SFINAE expression to help the compiler a bit
      - Deduce return type in body of function instead of using trailing
        return type, which the MSVC static analyzer fails to understand.
    
    Fixes #6

--- include/enum_set/magic/magic_enum_set.hpp
+++ include/enum_set/magic/magic_enum_set.hpp
@@ -30,8 +30,10 @@ struct magic_enum_set_factory
     static constexpr auto indices = std::make_index_sequence<values.size()>();
 
     template <size_t... Indices>
-    static auto make_type(std::index_sequence<Indices...>) noexcept
-        -> value_set<Enum, values[Indices]...>;
+    static constexpr auto make_type(std::index_sequence<Indices...>) noexcept
+    {
+        return value_set<Enum, values[Indices]...>();
+    }
 
     using type = decltype(make_type(indices));
 };
